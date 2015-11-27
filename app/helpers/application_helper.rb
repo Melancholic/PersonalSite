0cc380c09beb945b8a7a4c90206520fa9740df58
Andrey Nagorny <mail@anagorny.com>
2015-11-27 22:20:46 +0300

    Change title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,18 @@
 module ApplicationHelper
-  def set_i18n
-    #Custom user param
-    locale=  session[:locale] unless  session[:locale].nil?
-    #Default user param
-    #locale ||= current_user.locale if signed_in?
-    I18n.locale=locale unless locale.nil?
-  end
+    def full_title(page_title="")
+        base_title= "Nagorny Andrey"
+        if (page_title.empty?)
+          base_title
+        else
+          "#{base_title} | #{page_title}"
+        end
+    end
+
+    def set_i18n
+        #Custom user param
+        locale=  session[:locale] unless  session[:locale].nil?
+        #Default user param
+        #locale ||= current_user.locale if signed_in?
+        I18n.locale=locale unless locale.nil?
+    end
 end
