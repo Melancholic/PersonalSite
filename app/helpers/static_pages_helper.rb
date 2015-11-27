359ea2960cd4ea796d67f00e2af86ddf44552300
Andrey Nagorny <mail@anagorny.com>
2015-11-27 22:19:01 +0300

    Make anchors helpers

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -3,4 +3,30 @@ module StaticPagesHelper
 		image_tag("creative_template/#{arg}", param)
 	end
 
+	def contact_path 
+		root_path anchor: "contact"
+	end
+	def about_path 
+		root_path anchor: "about"
+	end
+	def services_path
+		root_path anchor: "services"
+	end
+	def portfolio_path
+		root_path anchor: "portfolio"
+	end
+	
+	def contact_url 
+		root_url anchor: "contact"
+	end
+	def about_url 
+		root_url anchor: "about"
+	end
+	def services_url
+		root_url anchor: "services"
+	end
+	def portfolio_url
+		root_url anchor: "portfolio"
+	end
+
 end
