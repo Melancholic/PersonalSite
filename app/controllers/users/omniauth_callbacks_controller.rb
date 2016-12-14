56784acc738d8e91c7c0c3d0bc815d3fa05a5a6b
nagorny <mail@anagorny.com>
2016-12-14 10:27:03 +0300

    Uncommited changes. SignIn via OmniAuth. User profile

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,7 +4,7 @@ def self.provides_callback_for(provider)
     class_eval %Q{
       def #{provider}
         @user = User.find_for_oauth(env["omniauth.auth"], current_user)
-
+        logger.info("\n\n\n from find: #{@user.try(:id)}\n\n\")
         if @user.persisted?
           sign_in_and_redirect @user, event: :authentication
           set_flash_message(:success, :success, kind: "#{provider}".capitalize) if is_navigational_format?
